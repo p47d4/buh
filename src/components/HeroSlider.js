5bc5acd820962ebe7aaf04991da03b956c510ba4
p47d4 <107918010+p47d4@users.noreply.github.com>
2022-09-30 13:02:17 +0100

    home-slider

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -8,7 +8,7 @@ const HeroSlider = () => {
                 <Carousel.Item interval={3000}>
                     <img
                         className="d-block w-100"
-                        src="assets/img/buh/4.jpg"
+                        src="assets/img/buh/home-slider/buh9.jpeg"
                         alt="First slide"
                         height='800px'
                     />
@@ -23,8 +23,83 @@ const HeroSlider = () => {
                 <Carousel.Item interval={3000}>
                     <img
                         className="d-block w-100"
-                        src="assets/img/buh/2.jpg"
+                        src="assets/img/buh/home-slider/buh10.jpeg"
                         alt="Second slide"
+                        height='800px'
+                    />
+                    <Carousel.Caption>
+                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            <a href="/patient/register"className="btn btn-secondary col">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-success col" target="_blank">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src="assets/img/buh/home-slider/buh13.jpeg"
+                        alt="Third slide"
+                        height='800px'
+                    />
+                    <Carousel.Caption>
+                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            <a href="/patient/register"className="btn btn-secondary col">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-success col" target="_blank">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src="assets/img/buh/1.jpg"
+                        alt="Fourth slide"
+                        height='800px'
+                    />
+                    <Carousel.Caption>
+                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            <a href="/patient/register"className="btn btn-secondary col">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-success col" target="_blank">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src="assets/img/buh/2.jpg"
+                        alt="Fifth slide"
+                        height='800px'
+                    />
+                    <Carousel.Caption>
+                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            <a href="/patient/register"className="btn btn-secondary col">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-success col" target="_blank">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src="assets/img/buh/home-slider/buh18.jpeg"
+                        alt="Sixth slide"
+                        height='800px'
+                    />
+                    <Carousel.Caption>
+                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            <a href="/patient/register"className="btn btn-secondary col">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-success col" target="_blank">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block w-100"
+                        src="assets/img/buh/home-slider/buh21.jpeg"
+                        alt="Seventh slide"
                         height='800px'
                     />
                     <Carousel.Caption>
