04ff74227229d16d66c0e27b8a460c6e03c2b43b
BilkisuT <bilkisutahirmamman@gmail.com>
2022-10-25 11:26:13 +0100

    heroslider

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -14,32 +14,75 @@ const HeroSlider = () => {
                     
                     <img 
                         className="d-block w-100"
-                        src="assets/img/buh/1.jpg"
+                        src="assets/img/home-slider/buhHERO1.jpeg"
                         alt="First slide"
                         height='800px'
                         filter='brightness(50%)'
                     />
                     <Carousel.Caption>
-                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <h4>Welcome to </h4>
+                        <h1><span>Baze University Hospital</span></h1> 
                         <div className="mx-4 row">
                             
-                            <a href="/patient/register"className="btn btn-outline-light col">Register</a>
-                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-outline-info col" target="_blank"rel="noreferrer">Watch Video</a>
+                            <a href="/patient/register"className="btn btn-outline-light col rounded-pill ">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-outline-info col rounded-pill" target="_blank"rel="noreferrer">Watch Video</a>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={3000}>
-                    <img
+                    
+                    <img 
                         className="d-block w-100"
-                        src="assets/img/buh/2.jpg"
-                        alt="Second slide"
-                        height='800px'
+                        src="assets/img/home-slider/buhHERO3.jpeg"
+                        alt="First slide"
+                        height='700px'
+                        filter='brightness(50%)'
                     />
                     <Carousel.Caption>
-                        <h1>Welcome to <span>Baze University Hospital</span></h1>
+                        <h4>Welcome to </h4>
+                        <h1><span>Baze University Hospital</span></h1>
                         <div className="mx-4 row">
-                            <a href="/patient/register"className="btn btn-outline-light col">Register</a>
-                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-outline-info col" target="_blank">Watch Video</a>
+                            <a href="/patient/register"className="btn btn-outline-light col rounded-pill">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-outline-info col rounded-pill" target="_blank"rel="noreferrer">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            
+
+                <Carousel.Item interval={3000}>
+                    
+                    <img 
+                        className="d-block w-100"
+                        src="assets/img/home-slider/buhHERO2.jpeg"
+                        alt="First slide"
+                        height='800px'
+                        filter='brightness(50%)'
+                    />
+                    <Carousel.Caption>
+                        <h4>Welcome to </h4>
+                        <h1><span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            <a href="/patient/register"className="btn btn-outline-light col rounded-pill">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-outline-info col rounded-pill" target="_blank"rel="noreferrer">Watch Video</a>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    
+                    <img 
+                        className="d-block w-100"
+                        src="assets/img/home-slider/buhHERO4.jpeg"
+                        alt="First slide"
+                        height='800px'
+                        filter='brightness(50%)'
+                    />
+                    <Carousel.Caption>
+                        <h4>Welcome to </h4>
+                        <h1><span>Baze University Hospital</span></h1>
+                        <div className="mx-4 row">
+                            
+                            <a href="/patient/register"className="btn btn-outline-light col rounded-pill">Register</a>
+                            <a href="https://www.youtube.com/watch?v=vunzMowSQAQ" className="btn btn-outline-info col rounded-pill" target="_blank"rel="noreferrer">Watch Video</a>
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
